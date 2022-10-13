485f42e47d032dca40425ec8037abf521dd77efc
SmallWatermelon <470544301@qq.com>
2022-10-13 21:11:43 +0800

    init springboot

---
+++ src/test/java/com/xxg/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxg.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
